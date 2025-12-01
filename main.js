a57a40d7e228a419b25a763b05721b830fcd7de6
HoàngIT <116326364+Hoangk2@users.noreply.github.com>
2025-12-01 23:36:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,142 @@
+const BASE_URL = "https://dummyjson.com/posts";
+
+const $ = document.querySelector.bind(document);
+const overlay = $(".overlay");
+const modal = $(".modal");
+const close = $(".close");
+const postList = $(".post-list");
+const inputEl = $(".search");
+
+//Contructor Modal Dialog
+function Modal(modal, closeEl, overlay) {
+  this.modal = modal;
+  this.closeEl = closeEl;
+  this.overlay = overlay;
+
+  this.open = function () {
+    this.modal.classList.remove("hidden");
+    this.closeEl.classList.replace("hidden", "flex");
+    this.overlay.classList.remove("hidden");
+  };
+  this.close = function () {
+    this.modal.classList.add("hidden");
+    this.closeEl.classList.replace("flex", "hidden");
+    this.overlay.classList.add("hidden");
+    $(".modal-title").innerHTML = "";
+    $(".modal-body").innerHTML = "Loading...";
+    $(".modal-body").classList.replace("text-left", "text-center");
+  };
+  this.displayContent = function (title, body) {
+    $(".modal-title").innerHTML = title;
+    $(".modal-body").classList.replace("text-center", "text-left");
+    $(".modal-body").innerHTML = body;
+  };
+}
+
+const modalDialog = new Modal(modal, close, overlay);
+//Chống XSS
+const escapeHTML = (value) => {
+  const div = document.createElement("div");
+  const text = document.createTextNode(value);
+  div.append(text);
+  return div.innerHTML;
+};
+
+const getDatas = async (endpoint) => {
+  try {
+    const res = await fetch(`${BASE_URL}${endpoint}`);
+    if (!res.ok) throw new Error("No data found"); //Kiểm tra trước khi dùng res.json
+    const datas = await res.json();
+    if(datas.posts) {
+      if (!datas.posts.length) throw new Error("No matching posts found");
+    }
+    return datas;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//Render
+const render = (datas) => {
+  const html = datas.posts
+    .map(
+      ({ title, body, id }) => `
+      <li class="post-list-item" data-id=${escapeHTML(id)}>
+        <h2 class="mb-3 text-xl font-bold">${escapeHTML(title)}</h2>
+        <p class="font-[400] mb-2 overflow-hidden text-ellipsis whitespace-nowrap">
+          ${escapeHTML(body)}
+        </p>
+        <div class="controls flex justify-between">
+          <div class="detail btn rounded-3xl">Xem chi tiết</div>
+          <div class="edit flex gap-2">
+            <div class="fix font-bold cursor-pointer">Sửa</div>
+            <div class="delete font-bold text-red-400 cursor-pointer">Xóa</div>
+          </div>
+        </div>
+      </li>  
+    `
+    )
+    .join("");
+  postList.innerHTML = html;
+};
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  try {
+    const datas = await getDatas("");
+    render(datas);
+  } catch (error) {
+    if (error instanceof TypeError) { //kiểm tra lỗi mạng
+      error.name = "Sorry";
+      error.message = "An error has occurred";
+    }
+    postList.innerHTML = escapeHTML(error);
+  }
+});
+
+//Xử lý các sự kiện click
+document.addEventListener("click", async (e) => {
+  try {
+    if (e.target.matches(".detail")) {
+      const idPost = e.target.closest(".post-list-item").dataset.id;
+      modalDialog.open();
+      const { title, body } = await getDatas(`/${idPost}`);
+      modalDialog.displayContent(title, body);
+    }
+    if (e.target.closest(".close") || e.target.matches(".overlay")) {
+      modalDialog.close();
+    }
+    if (e.target.matches(".oldest")) {
+      $(".header-controls").querySelectorAll(".btn").forEach(btn => btn.classList.remove("active"));
+      $(".oldest").classList.add("active");
+      const datas = await getDatas(`?sortBy=id&order=asc`);
+      render(datas);
+    }
+    if (e.target.matches(".latest")) {
+      $(".header-controls").querySelectorAll(".btn").forEach(btn => btn.classList.remove("active"));
+      $(".latest").classList.add("active");
+      const datas = await getDatas(`?sortBy=id&order=desc`);
+      render(datas);
+    }
+  } catch(error) {
+    if(error instanceof TypeError) {
+      error.name = "Sorry";
+      error.message = "An error has occurred";
+    }
+    postList.innerHTML = escapeHTML(error);
+  }
+});
+
+//Xử lý sự kiện search
+inputEl.addEventListener("input", async (e) => {
+  try {
+    let inputValue = inputEl.value;
+    const datas = await getDatas(`/search?q=${inputValue}`);
+    render(datas);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      error.name = "Sorry";
+      error.message = "An error has occurred";
+    }
+    postList.innerHTML = escapeHTML(error);
+  }
+});
